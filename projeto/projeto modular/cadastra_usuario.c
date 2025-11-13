b8e8da3cc59af60cab1491f63dea415d8a529386
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2025-11-13 17:39:19 -0300

    teste

--- projeto/projeto modular/cadastra_usuario.c
+++ projeto/projeto modular/cadastra_usuario.c
@@ -13,19 +13,18 @@ void cadastra_usuario(Usuario cadastrados[]){
             break;
         }
     }
-    printf("digite o nome do usuario:\n");
-    fgets(cadastrados[pos].nome, MAX, stdin);
-    remove_nova_linha(cadastrados[pos].nome);
-    converte_minusculas(cadastrados[pos].nome);
     printf("digite o CPF:\n");
     fgets(cpf, MAX, stdin);
     remove_nova_linha(cpf);
     for(i = 0; i < 1000; i++){
         if(strcmp(cpf, cadastrados[i].cpf) == 0){
             printf("CPF ja cadastrado.\n");
-            cadastrados[pos].nome[0] = '\0';
             return;
         }
     }
     strcpy(cadastrados[pos].cpf, cpf);
+    printf("digite o nome do usuario:\n");
+    fgets(cadastrados[pos].nome, MAX, stdin);
+    remove_nova_linha(cadastrados[pos].nome);
+    converte_minusculas(cadastrados[pos].nome);
 }
